79ea5a9ef53da5687722ef98b71d439d863b0da0
Adriano Ney Nascimento do Amarala <adriano.amaral147@gmail.com>
2019-12-05 21:03:45 -0300

    padrão State

---
+++ padroes/Workspace/State1/src/MarioPequeno.java
@@ -0,0 +1,28 @@
+
+public class MarioPequeno implements MarioState {
+
+	@Override
+	public MarioState pegarCogumelo() {
+		System.out.println("Mario Grande!");
+		return new MarioGrande();
+	}
+
+	@Override
+	public MarioState pegarFlor() {
+		System.out.println("Mario Grande com Fogo!");
+		return new MarioFogo();
+	}
+
+	@Override
+	public MarioState pegarPena() {
+		System.out.println("Mario Grande com Capa!");
+		return new MarioCapa();
+	}
+
+	@Override
+	public MarioState levarDano() {
+		System.out.println("Mario Morto, Game Over!");
+		return new MarioMorto();
+	}
+
+}
